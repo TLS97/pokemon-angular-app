573c2600a0d00b85af40ef40b7a00065cf0974c1
Tine-Lovise Storvoll <tinelovisestorvoll@gmail.com>
2023-01-31 09:22:42 +0100

    chore: create feature and page components

---
+++ src/app/components/collected-button/collected-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collected-button',
+  templateUrl: './collected-button.component.html',
+  styleUrls: ['./collected-button.component.css']
+})
+export class CollectedButtonComponent {
+
+}
