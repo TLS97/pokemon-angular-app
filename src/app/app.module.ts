8bacabf021be2b667ed64d99ed8434f9c0d46f9a
Tine-Lovise Storvoll <tinelovisestorvoll@gmail.com>
2023-01-31 16:18:31 +0100

    feat: add routing between pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -21,12 +22,10 @@ import { CollectedButtonComponent } from './components/collected-button/collecte
     LoginFormComponent,
     PokemonListComponent,
     PokemonListItemComponent,
-    CollectedButtonComponent
+    CollectedButtonComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
