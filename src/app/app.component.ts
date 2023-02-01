3364c399b858954cb5d531effab88631a80cb0a5
Tine-Lovise Storvoll <tinelovisestorvoll@gmail.com>
2023-02-01 15:49:13 +0100

    feat: start setting up the profile page and add the pokemon list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TrainerService } from './services/trainer.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'ng-pokemon';
+export class AppComponent implements OnInit {
+  constructor(private readonly trainerService: TrainerService) {}
+
+  ngOnInit(): void {
+    if (this.trainerService.trainer) {
+      // this.pokemonService.findAllPokemon();
+    }
+  }
 }
