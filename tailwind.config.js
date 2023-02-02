0b2a47ed24bb298dc0bed57f272fad0814290b61
Ha <Hahoang@live.no>
2023-02-02 15:05:24 +0100

    fix: Add login page styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
+    colors: {
+      purple: '#845EC2',
+      blue: '#3B82F6',
+      white: '#FFFFFF',
+
+    },
   },
   plugins: [],
 };
